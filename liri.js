f082bfeeaf457b02f4cd67318820ac87f9e039ba
coafer <mac@macs-MacBook-Pro.local>
2018-04-10 01:07:31 -0400

    Spotify completed part

--- liri.js
+++ liri.js
@@ -2,6 +2,8 @@
 require("dotenv").config();
 //required to import the keys.js file and store it in a variable
 var keys = require('./keys');
+//necessary npm modules
+var request = require('request');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 //var client = new Twitter(keys.twitter);
@@ -14,13 +16,18 @@ console.log(userInputMore);
 
 // `my-tweets`
 
-// `spotify-this-song`
+
 if (userInput === "spotify-this-song"){
+    // `spotify-this-song`
     //use the second command argv to search in spotify
     spoty(userInputMore);
+} else if(userInput === "movie-this"){
+    // `movie-this`
+    //movie API-request
+    movie(userInputMore);
 }
 
-// `movie-this`
+
 
 
 // `do-what-it-says`
@@ -30,9 +37,20 @@ function spoty(songName) {
     spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
-        }
+        } 
        
       //console.log(JSON.stringify(data, null, 2)); 
-      console.log(JSON.stringify(data.tracks.items[0].album.artists[0].external_urls));
+      //Artist(s)
+      console.log(JSON.stringify("Artist(s): " + data.tracks.items[0].album.artists[0].name));
+      //The song's name
+      console.log(JSON.stringify("The song's name: " + data.tracks.items[0].name));
+      //A preview link of the song from Spotify
+      console.log(JSON.stringify("A preview link of the song: " + data.tracks.items[0].preview_url));
+      //The album that the song is from
+      console.log(JSON.stringify("album: " + data.tracks.items[0].album.name));
       });
+}
+
+function movie(tittle){
+    console.log(tittle);
 }
